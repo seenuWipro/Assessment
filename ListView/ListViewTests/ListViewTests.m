91eefdb07658de73cbd98961e25fce54cd5cfca0
Seenu <>
2018-05-08 18:31:54 +0530

    Added unit test cases for tableview and cell

--- ListView/ListViewTests/ListViewTests.m
+++ ListView/ListViewTests/ListViewTests.m
@@ -7,9 +7,11 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "ViewController.h"
+#import "ItemTableViewCell.h"
 
 @interface ListViewTests : XCTestCase
-
+@property (nonatomic, strong) ViewController *vc;
 @end
 
 @implementation ListViewTests
@@ -17,11 +19,44 @@
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    self.vc = [storyboard instantiateViewControllerWithIdentifier:@"ViewController"];
+    [self.vc performSelectorOnMainThread:@selector(loadView) withObject:nil waitUntilDone:YES];
+}
+
+
+- (void)testTableViewCellCellReuseIdentifier
+{
+    ItemTableViewCell *cell = (ItemTableViewCell *)[self.vc tableView:self.vc.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]];
+    NSString *reuseIdentifier = @"ItemTableViewCell";
+    XCTAssertTrue([cell.reuseIdentifier isEqualToString:reuseIdentifier], @"Table does not create reusable cells");
+}
+
+- (void)testViewConformsToUITableViewDataSource
+{
+    XCTAssertTrue([self.vc conformsToProtocol:@protocol(UITableViewDataSource) ], @"View does not conform to UITableView datasource protocol");
+}
+
+- (void)testTableViewHasDataSource
+{
+    XCTAssertNotNil(self.vc.tableView.dataSource, @"Table datasource cannot be nil");
+}
+
+- (void)testViewConformsToUITableViewDelegate
+{
+    XCTAssertTrue([self.vc conformsToProtocol:@protocol(UITableViewDelegate) ], @"View does not conform to UITableView delegate protocol");
+}
+
+- (void)testTableViewIsConnectedToDelegate
+{
+    XCTAssertNotNil(self.vc.tableView.delegate, @"Table delegate cannot be nil");
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
+    self.vc = nil;
 }
 
 - (void)testExample {
